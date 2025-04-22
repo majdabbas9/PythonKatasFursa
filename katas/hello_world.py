a40b2f7922edd0e53905999bd7f14925b4cd4080
majdabbas9 <majd.abbas999@gmail.com>
2025-04-22 11:38:27 +0300

    hi

--- katas/hello_world.py
+++ katas/hello_world.py
@@ -1,7 +1,7 @@
 def hello_world():
     return "hello world"
 
-
 if __name__ == '__main__':
     result_str = hello_world()
+    print("hi")
     print(result_str)   # hello world should be printed
