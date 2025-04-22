a8febbcb405c963d5cc8a18799efce2c0128a6b8
majdabbas9 <majd.abbas999@gmail.com>
2025-04-22 13:19:01 +0300

    true counter solution

---
+++ katas/test/test_true_counter.py
@@ -0,0 +1,32 @@
+import unittest
+from katas.true_counter import count_true_values
+
+class TestTrueCount(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(count_true_values([True, True, True]), 3)
+
+    def test_2(self):
+        self.assertEqual(count_true_values([False, False, False]), 0)
+
+    def test_3(self):
+        self.assertEqual(count_true_values([True, False, True, False]), 2)
+
+    def test_4(self):
+        self.assertEqual(count_true_values([]), 0)
+
+    def test_5(self):
+        self.assertEqual(count_true_values([False, False, False]), 0)
+
+    def test_6(self):
+        self.assertEqual(count_true_values([True]), 1)
+
+    def test_7(self):
+        self.assertEqual(count_true_values([False]), 0)
+
+    def test_8(self):
+        self.assertEqual(count_true_values([False,False,False,True,True,False,True,False,False,True,True]), 5)    
+
+
+if __name__ == '__main__':
+    unittest.main()
