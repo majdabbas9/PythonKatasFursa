8bebba6f4c8fee39df660deff64fd8d18291b84f
majdabbas9 <majd.abbas999@gmail.com>
2025-04-22 20:22:33 +0300

    is unique string solution

--- katas/is_unique_str.py
+++ katas/is_unique_str.py
@@ -8,7 +8,15 @@ def is_unique(string):
     Returns:
         True if all characters are unique, False otherwise
     """
-    return True
+    bucket = [0] * 128
+    string = string.lower()
+    for ch in string:
+        ascii_val_of_ch = ord(ch)
+        bucket[ascii_val_of_ch]+=1
+    for b in bucket:
+        if b > 1 :
+            return False
+    return True    
 
 
 if __name__ == '__main__':
@@ -16,8 +24,9 @@ if __name__ == '__main__':
     test2 = "World"
     test3 = "Python"
     test4 = "Unique"
-
-    print(f'"{test1}" has all unique characters: {is_unique(test1)}')  # Should be False (has repeated 'l')
-    print(f'"{test2}" has all unique characters: {is_unique(test2)}')  # Should be True
-    print(f'"{test3}" has all unique characters: {is_unique(test3)}')  # Should be True
-    print(f'"{test4}" has all unique characters: {is_unique(test4)}')  # Should be True
+    test0 = "Aa"
+    print(f'"{test0}" has all unique characters: {is_unique(test0)}') 
+    #print(f'"{test1}" has all unique characters: {is_unique(test1)}')  # Should be False (has repeated 'l')
+    #print(f'"{test2}" has all unique characters: {is_unique(test2)}')  # Should be True
+    #print(f'"{test3}" has all unique characters: {is_unique(test3)}')  # Should be True
+    #print(f'"{test4}" has all unique characters: {is_unique(test4)}')  # Should be True
